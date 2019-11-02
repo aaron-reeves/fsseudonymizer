37d125b35e64a2373f19cc3c7d63393b14af6710
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2019-11-02 17:25:10 +0000

    Implemented custom pre-processing for FSS edition

--- customprocessing.cpp
+++ customprocessing.cpp
@@ -1,6 +1,6 @@
 /*
-Fsseudonymizer - customprocessing.h/cpp
----------------------------------------
+Fsseudonymizer - customprocessing.h/cpp - FSS edition
+-----------------------------------------------------
 Begin: 2019/10/13
 Author: Aaron Reeves <aaron.reeves@sruc.ac.uk>
 ---------------------------------------------------
@@ -13,18 +13,55 @@ Public License as published by the Free Software Foundation; either version 2 of
 
 #include "customprocessing.h"
 
+#include <ar_general_purpose/log.h>
+
+#include <epic_general_purpose/cph.h>
+#include <epic_general_purpose/cpostcode.h>
+
 int customPreProcessData( CTwoDArray<QVariant>* data ) {
-  Q_UNUSED( data )
+  if( data->hasColumnName( QStringLiteral( "CPH" ) ) ) {
+    data->appendColumn( QStringLiteral( "County" ) );
+  }
   
+  if( data->hasColumnName( QStringLiteral( "Producer Postcode" ) ) ) {
+    data->appendColumn( QStringLiteral( "Postcode district" ) );
+  }
+
   return ReturnCode::SUCCESS;
 }
 
 
 int customPreProcessRow( CTwoDArray<QVariant>* data, const int rowIdx ) {
-  Q_UNUSED( data )
-  Q_UNUSED( rowIdx )
+  int result = ReturnCode::SUCCESS; // Until shown otherwise
+  QString errMsg;
+
+  if( data->hasColumnName( QStringLiteral("CPH") ) ) {
+    CCPH cph( data->at( QStringLiteral("CPH"), rowIdx ).toString() );
+    if( !cph.isValid() ) {
+      result = ( result | ReturnCode::DATA_VALIDATION_PROBLEM );
+      errMsg = QStringLiteral( "Data does not appear to be a CPH number" );
+      // r + 2: add one for indexing, and add another one for the header row
+      logMsg( QStringLiteral( "Input file line %1, field '%2': %3" ).arg( rowIdx+2 ).arg( QStringLiteral("CPH"), errMsg ) );
+    }
+    else {
+      data->setValue( QStringLiteral("County"), rowIdx, cph.county() );
+    }
+  }
+
+  if( data->hasColumnName( QStringLiteral("Producer Postcode") ) ) {
+    CPostcode postcode( data->at( QStringLiteral("Producer Postcode"), rowIdx ).toString() );
+    if( !postcode.isValidFormat() ) {
+      result = ( result | ReturnCode::DATA_VALIDATION_PROBLEM );
+      errMsg = QStringLiteral( "Data does not appear to be a valid postcode" );
+      // r + 2: add one for indexing, and add another one for the header row
+      logMsg( QStringLiteral( "Input file line %1, field '%2': %3" ).arg( rowIdx+2 ).arg( QStringLiteral("Producer Postcode"), errMsg ) );
+    }
+    else {
+      data->setValue( QStringLiteral("Postcode district"), rowIdx, postcode.district() );
+    }
+  }
   
-  return ReturnCode::SUCCESS;
+  return result;
 }
 
 
