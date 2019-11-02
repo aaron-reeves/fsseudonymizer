902d6c29ba7a6f0c33346cbbf35188266df57e00
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2019-11-02 15:48:47 +0000

    Cosmetic changes for FSS edition

--- globals.h
+++ globals.h
@@ -7,7 +7,7 @@ class CGlobals {
   public:
     static QString OrgName() { return QStringLiteral( "SRUC Epidemiology Research Unit" ); }
     static QString OrgDomain() { return QStringLiteral( "epidemiology.sruc.ac.uk" ); }
-    static QString AppName() { return QStringLiteral( APP_NAME ); }
+    static QString AppName() { return QStringLiteral( "%1 (FSS Edition)" ).arg( QStringLiteral(APP_NAME) ); }
     static QString AppVersion() { return QStringLiteral( APP_VERSION ); }
 
     static QString CopyrightInfo() { return QStringLiteral( "Copyright (C) 2019 Scotland's Rural College (SRUC)" ); }
@@ -16,7 +16,7 @@ class CGlobals {
 
     static QString Author() { return QStringLiteral( "Aaron Reeves" ); }
     static QString EmailAddress() { return QStringLiteral( "Aaron.Reeves@sruc.ac.uk" ); }
-    static QString Website() { return QStringLiteral( "https://epidemiology.sruc.ac.uk/projects/software"); }
+    static QString Website() { return QStringLiteral( "https://epidemiology.sruc.ac.uk/projects/software/fss"); }
 
     static QString CopyrightNotice() { return QStringLiteral("%1.\n%2.\n\n%3.\n\n" ).arg( CopyrightInfo(), LicenseStatement(), AppDescr() ); }
 };
